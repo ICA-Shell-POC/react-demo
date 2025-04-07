ec94c9d3bf3d85f0f2f146da68690e7a60abcda3
DeviSovanDas <DeviSovanDas@WIN-OMFI987MUC4>
2025-04-07 11:56:08 +0530

    Refresh timestamp added

--- demo-app/src/components/dashboard/Dashboard.js
+++ demo-app/src/components/dashboard/Dashboard.js
@@ -8,18 +8,41 @@ const Dashboard = () => {
 
     const [data, setData] = useState();
     const [fuelData, setFuelData] = useState();
-
+    const [refreshMessage, setRefreshMessage] = useState("");
+    
     useEffect(() => {
+        let isIntervalRunning = false;
         const getData = async () => {
             const result = await fetchData('salestrends');
             setData(result);
+            const now = new Date();
+            setRefreshMessage("Data refreshed "+ now.toLocaleString());
+            setTimeout(() => setRefreshMessage(""), 5000);
         };
         const getFuelData = async () => {
             const result = await fetchData('fuelsales');
             setFuelData(result);
         }
+        if (!isIntervalRunning) {
         getData();
         getFuelData();
+        }
+        
+        const intervalData = setInterval(() => {
+            isIntervalRunning = true;
+            getData();
+        }, 1 * 60 * 1000);
+
+        const intervalFuelData = setInterval(() => {
+            isIntervalRunning = true;
+            getFuelData();
+        }, 1 * 60 * 1000);
+
+        return () => {
+            clearInterval(intervalData);
+            clearInterval(intervalFuelData);
+        };
+            
     }, []);
 
     if (!data) {
@@ -28,6 +51,14 @@ const Dashboard = () => {
 
     return (
         <div className="dashboard-container">
+            <div className='refresh-container'>
+                {refreshMessage && (
+                <h3 className="refresh-message">
+                    {refreshMessage}
+                </h3>
+                )}
+            </div>
+
             <div className="chart-grid">
                 <div className='chart-section'>
                     <h2 style={{ color: 'black' }}>Sales Data</h2>
